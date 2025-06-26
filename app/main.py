dad66b0f44222d349ad675550b835a20393e4270
sSPaiSs <naumenkoroma27@gmail.com>
2025-06-26 22:34:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list[int] | None = None) -> None:
+    def __init__(
+            self, name: str, weight: int, coords: list[int] | None = None
+    ) -> None:
         if coords is None:
             coords = [0, 0]
         self.name = name
@@ -28,7 +30,9 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list[int] | None = None) -> None:
+    def __init__(
+            self, name: str, weight: int, coords: list[int] | None = None
+    ) -> None:
         if coords is None:
             coords = [0, 0, 0]
         elif len(coords) == 2:
